5a74e6efa837aa9113036ea64b50a203f279ab54
JT Keller <jtkeller2@gmail.com>
2016-05-02 22:17:40 -0400

    Adding some cookie functionality.

--- src/main/java/com/thoughtstreamllc/courses/Main.java
+++ src/main/java/com/thoughtstreamllc/courses/Main.java
@@ -12,12 +12,16 @@ import static spark.Spark.post;
 public class Main {
     public static void main(String[] args) {
         get("/", ((request, response) -> {
-            return new ModelAndView(null, "index.hbs");
+            Map<String, String> model = new HashMap<>();
+            model.put("username", request.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
         }), new HandlebarsTemplateEngine());
 
         post("/sign-in", ((request, response) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", request.queryParams("username"));
+            String username = request.queryParams("username");
+            response.cookie("username", username);
+            model.put("username", username);
             return new ModelAndView(model, "sign-in.hbs");
         }), new HandlebarsTemplateEngine());
     }
